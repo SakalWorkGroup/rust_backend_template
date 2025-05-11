9f77496658da42a0e164019c4fe6f996148c2e25
TopMaster <topmaster98765@gmail.com>
2025-05-11 12:37:23 -0500

    add websocket feature

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ mod model;
 #[cfg(test)]
 mod test;
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer};
+use actix_web::{get, post, rt, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_ws::AggregatedMessage;
+use futures_util::StreamExt as _;
 use model::User;
-use mongodb::{bson::doc, options::IndexOptions, Client, Collection, IndexModel};
+use mongodb::{action::Aggregate, bson::doc, options::IndexOptions, Client, Collection, IndexModel};
 
 const DB_NAME: &str = "ActixTest";
 const COLL_NAME: &str = "users";
@@ -50,6 +52,36 @@ async fn create_username_index(client: &Client) {
         .expect("creating an index should succeed");
 }
 
+async fn echo(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> { // WebSocket handler
+    let (res, mut session, stream) = actix_ws::handle(&req, stream)?;
+
+    let mut stream = stream
+        .aggregate_continuations()
+        .max_continuation_size(2_usize.pow(20));
+
+    rt::spawn(async move {
+        while let Some(msg) = stream.next().await {
+            match msg {
+                Ok(AggregatedMessage::Text(text)) => {
+                    session.text(text).await.unwrap();
+                }
+
+                Ok(AggregatedMessage::Binary(bin)) => {
+                    session.binary(bin).await.unwrap();
+                }
+
+                Ok(AggregatedMessage::Ping(msg)) => {
+                    session.pong(&msg).await.unwrap();
+                }
+
+                _ => {}
+            }
+        }
+    });
+
+    Ok(res)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| "mongodb://localhost:27017".into());
@@ -62,6 +94,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(client.clone()))
             .service(add_user)
             .service(get_user)
+            .route("/echo", web::get().to(echo)) // WebSocket route
     })
     .bind("127.0.0.1:8080")?
     .run()
